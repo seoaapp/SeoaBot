eb0c02a530c0c1da2b45e3421ba1a66a44397ea6
ttakkku <ttakkku@outlook.com>
2019-06-06 14:29:33 +0900

    Add help  Command about '=search'

--- commands/help.js
+++ commands/help.js
@@ -8,6 +8,7 @@ exports.run = ((seoa, msg) => {
   let help = new discord.RichEmbed()
   .addField('Seoa CommandBook', 'Prefix: =')
   .addField('Help (CommandBook)', '=help / =도움 / =도움말')
+  .addField('search (CommandBook)', '=search')
   .addField('와! 심심해!', '=[할말]')
   .setDescription('이 봇은 베타입니다. 불안정하니 버그가 발생할경우 [이슈](https://github.com/ttakkku/SeoaBot/issues)에 알려주세요')
 
