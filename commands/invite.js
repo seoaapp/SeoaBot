2b08cd83928b71fb30b1820ab6b3a8facbbab2d4
ttakkku <ttakkku@outlook.com>
2019-06-14 00:55:11 +0900

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -5,11 +5,12 @@
 
 const discord = require('discord.js')
 
-exports.run = (seoa, msg, settings) => {
+exports.run = ((seoa, msg, settings) => {
   let Invite = new discord.RichEmbed()
      .setDescription('**[봇 초대링크!](https://discordapp.com/oauth2/authorize?client_id=584692085614182440&permissions=8&scope=bot)**')
   msg.channel.send(Invite)
-}
+});
+
 exports.callSign = ['invite', '봇초대링크', '초대링크', 'Invite']
 exports.helps = {
   description: "초대링크를 줍니다",
