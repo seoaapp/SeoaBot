41a12154289aa43c3746b981cc3e588c054b0099
ttakkku <ttakkku@outlook.com>
2019-06-14 00:33:08 +0900

    Add Command Invite

--- commands/Invite.js
+++ commands/Invite.js
@@ -1,1 +1,18 @@
+/**
+ * @name Seoa:Invite
+ * @description Bot Invite Command 
+ */
 
+
+const discord = require('discord.js')
+
+exports.run = ((seoa, msg, settings) => {
+  let Invite = new discord.RichEmbed()
+  .setDescription('**[봇 초대링크!](https://discordapp.com/oauth2/authorize?client_id=584692085614182440&permissions=8&scope=bot)**')
+   msg.author.send(Invite)
+})
+exports.callSign = ['invite', '봇초대링크', '초대링크', 'Invite']
+exports.helps = {
+  description: "초대링크를 줍니다",
+  uses: "~Invite"
+}
