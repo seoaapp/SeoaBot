f75a292eb5797bf19f9146d76c8ee6b523000cfe
ttakkku <ttakkku@outlook.com>
2019-06-14 00:58:54 +0900

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -9,7 +9,7 @@ exports.run = ((seoa, msg, settings) => {
   let invite = new discord.RichEmbed()
     .addField('Seoa CommandBook', 'Prefix: ~')
     .setDescription('**[봇 초대링크!](https://discordapp.com/oauth2/authorize?client_id=584692085614182440&permissions=8&scope=bot)**')
-  msg.channel.send(invite).then(err => console.log(err)
+  msg.channel.send(invite).then(err => console.log(err))
 });
 
 exports.callSign = ['invite', '봇초대링크', '초대링크', 'Invite']
