6494ac427c33f353ec4a60c592d8d14c41677c55
ttakkku <ttakkku@outlook.com>
2019-07-24 17:54:14 +0900

    Co-authored-by: PMH <kok4575@gmail.com>

--- commands/leave.js
+++ commands/leave.js
@@ -3,15 +3,15 @@
  * @description Voice Leave
  */
 
-exports.run = ((seoa, msg, settings) => { 
+exports.run = (seoa, msg, settings) => {
   if (msg.guild.voiceConnection) {
     msg.member.voiceChannel.leave()
     msg.channel.send('음성 채널을 떠났습니다!')
   }
-})
+}
 
-exports.callSign = ['나가기','leave']
+exports.callSign = ['나가기', 'leave']
 exports.helps = {
-    description: "음성 채널을 나갑니다",
-    uses: ">leave"
+  description: '음성 채널을 나갑니다',
+  uses: '>leave'
 }
