800ba83f279d491e43404b6803353fd57ac06cb7
Helloyunho <yunho050840@gmail.com>
2019-07-29 23:34:19 +0900

    Oops

---
+++ commands/point.js
@@ -0,0 +1,10 @@
+/**
+ * @name Seoa:helps
+ * @description Help Command
+ */
+
+exports.callSign = ['quiz point', '퀴즈 포인트']
+exports.helps = {
+  description: '퀴즈 포인트를 볼 수 있습니다.',
+  uses: '>quiz point'
+}
