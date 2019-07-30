7427726781d823edd16444eee9ca7872bb85ae09
ttakkku <ttakkku@outlook.com>
2019-07-30 22:33:35 +0900

    Fix settings

--- commands/settings.js
+++ commands/settings.js
@@ -7,20 +7,24 @@ const fs = require('fs')
 
 exports.run = (seoa, msg, settings, query) => {
   // TODO: 대충 언어설정이랑 서버장일경우 공지 채널 설정하는 내용
-  if (['lang', '언어'].includes(query.args[0].toLowerCase())) {
-    if (!(query.args[1] === 'kr' || query.args[1] === 'en')) {
-      settings.servers[msg.guild.id].lang = 'en'
+  if (query.args[0]) {
+    if (['lang', '언어'].includes(query.args[0].toLowerCase())) {
+      if (!(query.args[1] === 'kr' || query.args[1] === 'en')) {
+        settings.servers[msg.guild.id].lang = 'en'
+        fs.writeFileSync('./ServerData/servers.json', JSON.stringify(settings.servers, null, '  '))
+        msg.channel.send('해당 언어는 정의되어 있지 않아 `en`으로 설정되었습니다.')
+      } else {
+        settings.servers[msg.guild.id].lang = query.args[1]
+        fs.writeFileSync('./ServerData/servers.json', JSON.stringify(settings.servers, null, '  '))
+        msg.channel.send(`\`${query.args[1]}\`` + '으로 설정되었습니다.')
+      }
+    } else if (['channel', '채널'].includes(query.args[0].toLowerCase())) {
+      settings.servers[msg.guild.id].channelnoticeid = msg.mentions.channels.firstKey()
       fs.writeFileSync('./ServerData/servers.json', JSON.stringify(settings.servers, null, '  '))
-      msg.channel.send('해당 언어는 정의되어 있지 않아 `en`으로 설정되었습니다.')
-    } else {
-      settings.servers[msg.guild.id].lang = query.args[1]
-      fs.writeFileSync('./ServerData/servers.json', JSON.stringify(settings.servers, null, '  '))
-      msg.channel.send(`\`${query.args[1]}\`` + '으로 설정되었습니다.')
+      msg.channel.send('성공!')
     }
-  } else if (['channel', '채널'].includes(query.args[0].toLowerCase())) {
-    settings.servers[msg.guild.id].channelnoticeid = msg.mentions.channels.firstKey()
-    fs.writeFileSync('./ServerData/servers.json', JSON.stringify(settings.servers, null, '  '))
-    msg.channel.send('성공!')
+  } else {
+    msg.channel.send('`채널, 언어` 중에서 하나를 골라주세요')
   }
 }
 
