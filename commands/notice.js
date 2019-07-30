4c7a2654f488851b6e1cc60396166afbb1ee4bfe
ttakkku <ttakkku@outlook.com>
2019-07-30 22:51:00 +0900

    Update notice.js

--- commands/notice.js
+++ commands/notice.js
@@ -8,11 +8,11 @@ exports.run = (seoa, msg, settings, query) => {
     seoa.guilds.forEach((guild) => {
       if (settings.servers[guild.id].channelnoticeid === '') {
         // 서버장에게 전송
-        guild.owner.send('<@' + guild.ownerID + '> 공지 채널 설정이 되지 않았습니다!\n> 서버장에게 공지를 전송합니다.\n\n' + query.args.toLowerCase())
+        guild.owner.send('<@' + guild.ownerID + '> 공지 채널 설정이 되지 않았습니다!\n> 서버장에게 공지를 전송합니다.\n\n' + query.args)
       } else {
         /* 설정된 채널로 보내기 */
         if (guild.channels.get(settings.servers[guild.id].channelnoticeid)) {
-          guild.channels.get(settings.servers[guild.id].channelnoticeid).send(query.args[0])
+          guild.channels.get(settings.servers[guild.id].channelnoticeid).send(query.args)
         }
         console.log(guild.channels.get(settings.servers[guild.id].channelnoticeid))
       }
