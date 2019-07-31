2252b72bd54add2c50726c6f8666f91fcfd48062
ttakkku <ttakkku@outlook.com>
2019-07-31 17:08:36 +0900

    Fix kor.json

--- commands/quiz.js
+++ commands/quiz.js
@@ -43,7 +43,7 @@ exports.run = (seoa, msg, settings) => {
         .setColor(0x0000ff)
         .setAuthor(locale[settings.servers[msg.guild.id].lang]['QUIZ2'].replace('[msg.author.username]', msg.author.username), msg.author.displayAvatarURL)
         .setTitle('Quiz No.' + quizNum)
-        .addField('Q. ' + QuizData[quizNum].question.replace('{username}', msg.author.username), '제한시간은 **1분**입니다.')
+        .addField('Q. ' + QuizData[quizNum].question.replace('{username}', msg.author.username), locale[settings.servers[msg.guild.id].lang].min)
       if (QuizData[quizNum].image) {
         quizEmbed.setImage(QuizData[quizNum].image)
       }
@@ -68,7 +68,7 @@ exports.run = (seoa, msg, settings) => {
             const quizFailByLate = new discord.RichEmbed()
               .setColor(0x808080)
               .setDescription(locale[settings.servers[msg.guild.id].lang]['QUIZMSG1'])
-              .setAuthor(msg.author.username + '님 - 타임 오버입니다!', msg.author.displayAvatarURL)
+              .setAuthor(locale[settings.servers[msg.guild.id].lang].Over.replace('[msg.author.username]', msg.author.username), msg.author.displayAvatarURL)
               .setTitle('Quiz No.' + quizNum)
               .addField('Q. ' + QuizData[quizNum].question.replace('{username}', msg.author.username), '**A.** ' + QuizData[quizNum].explanation)
             if (QuizData[quizNum].image) {
@@ -90,8 +90,8 @@ exports.run = (seoa, msg, settings) => {
             if (collected.array()[0].emoji.name === QuizAwnser) {
               const quizCorrectEmbed = new discord.RichEmbed()
                 .setColor(0x00ff00)
-                .setDescription('[Code Quiz 오류 제보하러 가기!](https://github.com/seoaapp/SeoaBot/issues/new/choose)')
-                .setAuthor(msg.author.username + '님 - 정답입니다!', msg.author.displayAvatarURL)
+                .setDescription(locale[settings.servers[msg.guild.id].lang].IS)
+                .setAuthor(locale[settings.servers[msg.guild.id].lang].SUS.replace('[msg.author.username]', msg.author.username), msg.author.displayAvatarURL)
                 .setTitle('Quiz No.' + quizNum)
                 .addField('Q. ' + QuizData[quizNum].question.replace('{username}', msg.author.username), '**A.** ' + QuizData[quizNum].explanation)
               if (QuizData[quizNum].image) {
@@ -103,8 +103,8 @@ exports.run = (seoa, msg, settings) => {
             } else { // 틀렸을 경우
               const quizNotCorrectEmbed = new discord.RichEmbed()
                 .setColor(0xff0000)
-                .setDescription('[Code Quiz 문의하러 가기!](https://github.com/seoaapp/SeoaBot/issues/new/choose)')
-                .setAuthor(msg.author.username + '님 - 오답입니다!', msg.author.displayAvatarURL)
+                .setDescription(locale[settings.servers[msg.guild.id].lang].PR)
+                .setAuthor(locale[settings.servers[msg.guild.id].lang].NOTCORRET.replace('[msg.author.username]', msg.author.username), msg.author.displayAvatarURL)
                 .setTitle('Quiz No.' + quizNum)
                 .addField('Q. ' + QuizData[quizNum].question.replace('{username}', msg.author.username), '**A.** ' + QuizData[quizNum].explanation)
               if (QuizData[quizNum].image) {
--- commands/search.js
+++ commands/search.js
@@ -6,17 +6,24 @@
 const ytSearch = require('yt-search')
 const randomHexColor = require('random-hex-color')
 
-exports.run = (seoa, msg) => {
+/** Message */
+const locale = {
+  en: require('../locales/en.json'),
+  kor: require('../locales/kor.json'),
+  pt: require('../locales/pt.json')
+}
+
+exports.run = (seoa, msg, settings) => {
   /** Message Filter for .awaitMessages() */
   const filter = (m) => m.author.id === msg.author.id
 
-  msg.channel.send('YouTube에서 검색할 검색어를 입력해 주세요 (제한시간 1분) **[Dev]**').then((m) => {
+  msg.channel.send(locale[settings.servers[msg.guild.id].lang].YOUTUBEDEVMSG).then((m) => {
     msg.channel.awaitMessages(filter, {
       max: 1,
       time: 60000
     }).then((collect) => {
       if (!collect.first()) {
-        m.edit('1분이 지나 취소되었습니다')
+        m.edit(locale[settings.servers[msg.guild.id].lang].Corrent)
         m.delete(2000)
       } else {
         ytSearch(collect.first().content, (err, res) => {
@@ -24,13 +31,13 @@ exports.run = (seoa, msg) => {
 
           const embed = {
             color: parseInt(randomHexColor().substring(1), 16),
-            title: '\'' + collect.first().content.slice(0, 200) + '\'의 검색 결과',
+            title: '\'' + collect.first().content.slice(0, 200) + locale[settings.servers[msg.guild.id].lang].search,
             fields: []
           }
           res.videos.slice(0, 10).forEach((video, index) => {
             embed.fields.push({
               name: (index + 1) + '. ' + video.title,
-              value: '[보기](http://youtube.com' + video.url + ') | 길이: ' + video.duration + ' | 게시일: ' + video.ago + ' | ' + video.views + '번 재생됨'
+              value: locale[settings.servers[msg.guild.id].lang].view.replace('[video.url]', video.url).replace('[video.duration]', video.duration).replace('[video.ago]', video.ago).replace('[video.views]', video.views)
             })
           })
           m.edit({ embed })
@@ -39,7 +46,7 @@ exports.run = (seoa, msg) => {
             time: 60000
           }).then((collect2) => {
             if (!collect2.first()) {
-              m.edit('1분이 지나 취소되었습니다')
+              m.edit(locale[settings.servers[msg.guild.id].lang].Corrent)
               m.delete(2000)
             } else {
               if (collect2) {
