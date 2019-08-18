47f376b09a0ccb5c008c295523cae2d3088462f4
Lee Hyun <ink0416@naver.com>
2019-08-18 22:11:55 +0900

    난독화 해제 및 eval 보안 정책 업데이트

--- commands/dev.js
+++ commands/dev.js
@@ -6,15 +6,33 @@
 /** Message */
 const i18n = require('i18n')
 
+/** 제한 */
+const unObfuscate = require('./stuffs/unObfuscate')
+const risks = ['delete', 'Obfuscate', 'risks', 'policy', 'setInterval', 'fs', 'require', 'eval', '\'+', '+\'', '"+', '+"', '`+', '+`', 'token', 'Function', 'exit', 'pid', 'kill', 'Buffer', 'proto', 'debugger' ]
+const classes = ['Math', 'zlib', 'while', 'with', 'Timeout', 'decoder', 'encoder', 'stream', 'switch', 'root', 'repl', 'readline', 'tty', 'tls', 'finally', 'catch', 'try', 'this', 'querystring', 'Enumerable', 'path', 'Int', 'Float', 'os', 'net', 'new', 'module', 'Of', 'spector', 'of', 'in', 'import', 'if', 'https', 'http2', 'http', 'Property', 'function', 'for', 'true', 'false', 'events', 'eval', 'escape', 'export', 'exports', 'dgram', 'domain', 'dns', 'component', 'do', 'delete', 'default', 'constructor', 'Immediate', 'Interval', 'console', 'buffer', 'process', 'assert', 'Array', 'Buffer', 'Atomics', 'BigInt', 'Boolean', 'REQUEST', 'RESPONSE', 'END', 'CONNECTION', 'View', 'Date', 'Error', 'Function', 'GLOBAL', 'Intl', 'JSON', 'Map', 'NaN', 'Number', 'Object', 'Promise', 'Proxy', 'Reflect', 'RegExp', 'Set', 'String', 'Symbol', 'URL', 'Params', 'Assembly']
+classes.forEach(c => {
+  risks.push(`${c}=`)
+})
+
 exports.run = async (seoa, msg, query) => {
+  let policy = seoa.settings.localPolicy.eval
   let server = await seoa.db.select('serverdata', { id: msg.guild.id })
   server = server[0]
   if (seoa.settings.owners.includes(msg.author.id)) {
     try {
       // eslint-disable-next-line
-      eval(query.args.join(' '))
-    } catch (error) {
-      msg.channel.send(error)
+      let q = query.args.join(' ')
+      if (unObfuscate.detect(q)) {
+        if (!policy.acceptObfuscate) return msg.channel.send('보안 정책상 난독화된 코드를 사용할 수 없습니다.')
+        if (policy.decryptObfuscate) q = unObfuscate.unpack(q)
+      }
+      if (policy.disableRisks) return eval(q)
+      for (let i = 0; i < risks.length; i++) {
+        if (q.split(' ').join('').includes(risks[i])) return msg.channel.send(`보안 정책상 봇 구동에 영향이 가는 코드(${policy.sayFindedRisk ? risks[i]: '보안 정책으로 비공개'})를 사용할 수 없습니다.`)
+        if (i === risks.length - 1) eval(q)
+      }
+    } catch (e) {
+      msg.channel.send(policy.sayError ? e.toString() : '보안 정책상 에러를 표시하지 않습니다.')
     }
   } else {
     msg.channel.send(
