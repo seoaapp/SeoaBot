79b3a89bb5f6dd3815220ba760978bcd7d6f60f4
Lee Hyun <ink0416@naver.com>
2019-08-23 20:21:42 +0900

    무언가...?
    실수를 해버렸따★ 데헷

--- classes/seoa.js
+++ classes/seoa.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js')
 const LMusic = require('./music_lavalink')
 const Music = require('./music_native')
 const Search = require('./search')
+const { join } = require('path')
 const { PlayerManager } = require('discord.js-lavalink')
 
 module.exports = class Seoa extends Discord.Client {
@@ -25,15 +26,14 @@ module.exports = class Seoa extends Discord.Client {
       if (this.db) return this.db.update('serverdata', { owner: guild.ownerID }, { id: guild.id })
     })
 
-    const commands = require(config.commands)
+    const commands = require(join(__dirname, '../', config.commands))
     Object.keys(commands).forEach(k => {
       const command = commands[k]
-      if (config.localPolicy.core.printLoadedCMD) console.log(`${config.commands}/${k} loaded`)
+      if (config.localPolicy.core.printLoadedCMD) console.log(`${config.commands.split(/\\|\//).pop()}.${k} loaded`)
       command.callSign.forEach(c => {
         this.commands.set(c, command)
       })
     })
-
     this.login(config.token)
   }
 
