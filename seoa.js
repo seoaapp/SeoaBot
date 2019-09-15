0a2247b443c9f3b54ad8eb8539d819b6b568f780
Lee Hyun <ink0416@naver.com>
2019-09-15 14:57:20 +0900

    reload 명령어 추가

--- seoa.js
+++ seoa.js
@@ -2,39 +2,46 @@ const { Client } = require('discord.js')
 const UlangTS = require('ulangts')
 const UmusicTS = require('umusicts')
 const Umysql = require('umysql')
+const path = require('path')
 
 class Seoa extends Client {
   constructor (config) {
     super()
     this.config = UlangTS.parse(config, config)
-    this.classes = require(this.config.path.classes)
-    this.events = require(this.config.path.events)
-    this.commands = new Map()
-    this.commandsOrigin = require(this.config.path.commands)
-    
+    this.loadKeys = ['classes', 'events', 'commandsOrigin']
+    this.load()
+
     this.langs = new UlangTS(require(this.config.path.languages))
     this.m = new UmusicTS()
-    this.search = this.classes.search && this.config.api.youtube.enable ?
-      new this.classes.search(this.config.api.youtube.clientID).addParam('type', 'video').addParam('order', 'relevance') :
-      false
-    this.db = this.config.mysql.enable ?
-      new Umysql(this.config.mysql.host, this.config.mysql.user, this.config.mysql.pass, this.config.mysql.database, this.config.mysql.port, this.config.mysql.charset):
-      null
-    this.gi = this.db ? new this.classes.getinf(this.db, 'userdata', 'serverdata', 'mylist') : null
+    this.search = this.classes.Search && this.config.api.youtube.enable
+      ? new this.classes.Search(this.config.api.youtube.clientID).addParam('type', 'video').addParam('order', 'relevance')
+      : false
+    this.db = this.config.mysql.enable
+      ? new Umysql(this.config.mysql.host, this.config.mysql.user, this.config.mysql.pass, this.config.mysql.database, this.config.mysql.port, this.config.mysql.charset)
+      : null
+    this.gi = this.db && this.classes.GetInf ? new this.classes.GetInf(this.db, 'userdata', 'serverdata', 'mylist') : null
+    this.login(this.config.token)
+  }
 
-    Object.values(this.events).forEach(_ => new _(this))
+  load (loadKeys) {
+    (loadKeys || this.loadKeys).forEach(lK => {
+      delete require.cache[path.resolve(this.config.path[lK], './index.js')]
+      const temp = require(this.config.path[lK])
+      Object.keys(temp).forEach(k => {
+        const p = path.resolve(this.config.path[lK], `./${k}.js`)
+        delete require.cache[p]
+      })
+      this[lK] = require(this.config.path[lK])
+    })
+
+    this.commands = new Map()
+    Object.values(this.removeAllListeners().events).forEach(_ => new _(this))
     Object.values(this.commandsOrigin).forEach(_ => {
       const command = new _()
       command.alias.forEach(alia => {
         this.commands.set(alia, command)
       })
     })
-    
-    this.login(this.config.token)
-  }
-
-  dummy () {
-    return true
   }
 }
 
